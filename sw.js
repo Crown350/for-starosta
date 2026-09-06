e967564230d20940c55ba8ee40b8a16304e724c1
Crown350 <107409470+Crown350@users.noreply.github.com>
2026-09-06 19:22:22 +0300

    Update cache version from v3 to v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'starosta-v3';
+const CACHE = 'starosta-v4';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
