229ddd6e54049d3e3d1b6ad918924fdcaf255128
Crown350 <107409470+Crown350@users.noreply.github.com>
2026-09-06 13:13:55 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE = 'starosta-v1';
+const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+
+  // Файлы приложения: сначала кэш, потом сеть.
+  if (new URL(req.url).origin === location.origin) {
+    e.respondWith(
+      caches.match(req).then(hit => hit || fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(req, copy));
+        return res;
+      }).catch(() => caches.match('./index.html')))
+    );
+    return;
+  }
+
+  // Библиотека распознавания и словари: качаются один раз, дальше из кэша.
+  if (/jsdelivr\.net|tessdata/.test(req.url)) {
+    e.respondWith(
+      caches.match(req).then(hit => hit || fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(req, copy));
+        return res;
+      }))
+    );
+  }
+});
